009a8437f3bd20f0ba64ed19b1ff233ac067a210
izzzuuu <69377807+izzzuuu@users.noreply.github.com>
2020-08-08 10:38:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyAhwCTl1jW-nkrxVakSawXVNUCAA9zQd-s",
+  authDomain: "mufaiz-hack-worldx.firebaseapp.com",
+  databaseURL: "https://mufaiz-hack-worldx.firebaseio.com",
+  projectId: "mufaiz-hack-worldx",
+  storageBucket: "mufaiz-hack-worldx.appspot.com",
+  messagingSenderId: "661436799071",
+  appId: "1:661436799071:web:77c33d23dbe3a073b963ea",
+  measurementId: "G-ZR5VSWTS0E"
 };
 
      
